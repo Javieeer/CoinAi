5ea732fa85503f049807610324c953d06f6f7fb3
Javier Zapata <zapatajavier34@gmail.com>
2026-07-06 11:25:59 -0500

    Define docker structure

---
+++ backend/api/src/test/java/com/coinai/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
